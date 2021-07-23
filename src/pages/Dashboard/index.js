aa90472eb0ccb5a6d6a8e2c778cc630b53175ed0
jaydeep-concetto <jrv238@gmail.com>
2021-07-23 18:56:10 +0530

    ohif implement

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,41 @@
 import React, { Fragment, useState, useEffect } from "react";
 import "./style.css";
+const containerId = 'jd'
+const componentRenderedOrUpdatedCallback = function () {
+  console.log('OHIF Viewer rendered/updated');
+};
 
 let Dashboard = (props) => {
-  
+  const [d, setD] = useState(false);
+  var a;
+  useEffect(() => {
+    setTimeout(function () {
+      a = window.OHIFViewer.installViewer(
+        {
+          routerBasename: '/',
+          servers: {
+            dicomWeb: [
+              {
+                name: 'DCM4CHEE',
+                wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
+                qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+                wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
+                qidoSupportsIncludeField: true,
+                imageRendering: 'wadors',
+                thumbnailRendering: 'wadors',
+              },
+            ],
+          },
+        }, containerId, componentRenderedOrUpdatedCallback);
+      setD(true);
+    }, 1000);
+  }, []);
   return (
     <div className="container-fluid dashboardPage">
-      dfgfhgjh
-      </div>
+      <div id='jd' />
+      {d && <script>{a}
+      </script>}
+    </div>
   );
 };
 export default Dashboard;

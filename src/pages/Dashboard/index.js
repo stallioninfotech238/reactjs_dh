d7bc00f7cefcfbcb2854863096ea4c7eb84e8594
stallioninfotech238 <58873663+stallioninfotech238@users.noreply.github.com>
2021-07-26 23:36:51 +0530

    jul 26

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,40 +1,11 @@
 import React, { Fragment, useState, useEffect } from "react";
 import "./style.css";
-const containerId = 'jd'
-const componentRenderedOrUpdatedCallback = function () {
-  console.log('OHIF Viewer rendered/updated');
-};
 
 let Dashboard = (props) => {
-  const [d, setD] = useState(false);
-  var a;
-  useEffect(() => {
-    setTimeout(function () {
-      a = window.OHIFViewer.installViewer(
-        {
-          routerBasename: '/',
-          servers: {
-            dicomWeb: [
-              {
-                name: 'DCM4CHEE',
-                wadoUriRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/wado',
-                qidoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
-                wadoRoot: 'https://server.dcmjs.org/dcm4chee-arc/aets/DCM4CHEE/rs',
-                qidoSupportsIncludeField: true,
-                imageRendering: 'wadors',
-                thumbnailRendering: 'wadors',
-              },
-            ],
-          },
-        }, containerId, componentRenderedOrUpdatedCallback);
-      setD(true);
-    }, 1000);
-  }, []);
+  
   return (
     <div className="container-fluid dashboardPage">
-      <div id='jd' />
-      {d && <script>{a}
-      </script>}
+     
     </div>
   );
 };
